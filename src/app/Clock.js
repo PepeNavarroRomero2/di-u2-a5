17ad551850a8261097e7a84adfd8a55e264916bd
Pepe <josenavarrosegundodam@gmail.com>
2024-11-05 13:45:07 +0100

    Ejer1

--- src/app/Clock.js
+++ src/app/Clock.js
@@ -1,12 +1,8 @@
 export default function Clock({ time }) {
-    let hours = time.getHours();
-    if (hours >= 0 && hours <= 6) {
-      document.getElementById('time').className = 'night';
-    } else {
-      document.getElementById('time').className = 'day';
-    }
+    const hours = time.getHours();
+    const className = hours >= 0 && hours <= 6 ? 'night' : 'day';
     return (
-      <h1 id="time">
+      <h1 id="time" className={className}>
         {time.toLocaleTimeString()}
       </h1>
     );
